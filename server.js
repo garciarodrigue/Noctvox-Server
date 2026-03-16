72fb9f6b5f443dadd7410e9108ebb402ed5cb32b
Tenshi Kira <garciarodrigueza587@gmail.com>
2026-03-16 02:20:47 -0600

    page speak ai

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 import Fastify from "fastify";
+import path from "path";
+import fastifyStatic from "@fastify/static";
+
 import aiRoute from "./routes/aiRoute.js";
 
 const fastify = Fastify({logger:true});
 
-fastify.get("/", async ()=>{
-  return {status:"online"};
-});
-
-fastify.get("/ping", async ()=>{
-  return {pong:true};
+fastify.register(fastifyStatic,{
+root:path.join(process.cwd(),"public")
 });
 
 fastify.register(aiRoute);
 
 const start = async ()=>{
 
-  const port = process.env.PORT || 3000;
-
-  await fastify.listen({
-    port,
-    host:"0.0.0.0"
-  });
+await fastify.listen({
+port:process.env.PORT || 3000,
+host:"0.0.0.0"
+});
 
 };
 

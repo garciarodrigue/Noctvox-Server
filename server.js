707138956f65436a1027d107fc1e5aae1a6072ea
Tenshi Kira <garciarodrigueza587@gmail.com>
2026-03-16 03:52:15 -0600

    route / get

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 import Fastify from "fastify";
 import aiRoute from "./routes/aiRoute.js";
-
 import { connectDB } from "./database/mongo.js";
 
-const fastify = Fastify({logger:true});
+const fastify = Fastify({ logger: true });
+
+/* RUTA PRINCIPAL */
+
+fastify.get("/", async () => {
+  return {
+    status: "online",
+    core: "SYPH // NVX",
+    message: "Servidor activo"
+  };
+});
+
+/* RUTA PING */
+
+fastify.get("/ping", async () => {
+  return { pong: true };
+});
+
+/* RUTA IA */
 
 fastify.register(aiRoute);
 
-const start = async()=>{
+const start = async () => {
 
-await connectDB();
+  await connectDB();
 
-await fastify.listen({
-port:process.env.PORT || 3000,
-host:"0.0.0.0"
-});
+  await fastify.listen({
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0"
+  });
 
 };
 
